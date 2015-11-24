3876e27e83e7783bba66ee1b8f78cfa9cb9f31e4
JoeFranklin <joe.franklin@gmail.com>
2015-11-23 21:30:29 -0600

    added Clients controller with default methods and views to list clients

--- AssessmentRelations/Models/Client.cs
+++ AssessmentRelations/Models/Client.cs
@@ -17,7 +17,7 @@ namespace AssessmentRelations.Models
 
         public int Id { get; set; }
         public string Name { get; set; }
-        ICollection<Project> Projects { get; set; }
+        public virtual ICollection<Project> Projects { get; set; }
 
         public void AddProject(Project p)
         {
